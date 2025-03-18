e8d177ba24e67a89b89f96772638bef642e6d0bb
JeffreyJoel <jowoloko@gmail.com>
2025-03-18 02:12:46 +0100

    fix deployment issue

--- packages/server/pages/api/sponsor-transaction.ts
+++ packages/server/pages/api/sponsor-transaction.ts
@@ -39,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         // Return the sponsored transaction
         res.status(200).json({
             status: 'ok',
-            transaction: sponsoredTransaction.toString('base64'),
+            transaction: sponsoredTransaction.toString(),
             sponsorSignature: base58.encode(sponsorSignature)
         });
 
